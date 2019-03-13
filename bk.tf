3f5d9bfe7803232c84075106e02744bc0a96fcc8
C5278791 <bandeep.dhillon@sap.com>
2019-03-13 12:43:50 -0700

    test

--- bk.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket      = "ban-terraform2"
-    credentials = "/var/accounts/account-cobalt.json"
-    prefix      = "test-terraform-influx"
-  }
-}
--- vars.tf
+++
@@ -1,3 +0,0 @@
-variable "influxdb_url" {
-
-}
